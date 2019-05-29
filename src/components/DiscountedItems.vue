e679c3671ea5edc0da59af0db4d89a4036da0bcf
adamfdhl <adamfdhl@gmail.com>
2019-05-29 14:38:45 +0700

    add some styles in DiscountedItems

--- src/components/DiscountedItems.vue
+++ src/components/DiscountedItems.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <h2>Ramadhan sale! From 30% up to 85%!</h2>
-    <div class="items-container">
+    <div class="card-carousel-wrapper">
+      <div class="card-carousel--nav__left"/>
+
       <CardItem v-for="product in discountedItems" :key="product.id">
         <template v-slot:itemName>
           <h3>{{ product.title }}</h3>
@@ -10,6 +12,8 @@
           <p>{{ product.body | snippets }}</p>
         </template>
       </CardItem>
+
+      <div class="card-carousel--nav__right"/>
     </div>
   </div>
 </template>
@@ -33,16 +37,53 @@ export default {
   created() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
       this.discountedItems = response.data;
-      this.discountedItems = this.discountedItems.slice(0, 10);
+      this.discountedItems = this.discountedItems.slice(0, 5);
     });
   }
 };
 </script>
 <style lang="scss" scoped>
-.items-container {
+.card-carousel-wrapper {
   display: flex;
-  justify-content: center;
   align-items: center;
-  background-color: #f8f8f8;
+  justify-content: center;
+  margin: 20px 0 40px;
+  color: #666a73;
+
+  &--overflow-container {
+    overflow: hidden;
+  }
+
+  &--nav__left,
+  &--nav__right {
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    padding: 10px;
+    box-sizing: border-box;
+    border-top: 2px solid #42b883;
+    border-right: 2px solid #42b883;
+    cursor: pointer;
+    margin: 0 10px;
+    transition: transform 150ms linear;
+
+    &[disabled] {
+      opacity: 0.2;
+      border-color: black;
+    }
+  }
+
+  &--nav__left {
+    transform: rotate(-135deg);
+    &:active {
+      transform: rotate(-135deg) scale(0.9);
+    }
+  }
+  &--nav__right {
+    transform: rotate(45deg);
+    &:active {
+      transform: rotate(45deg) scale(0.9);
+    }
+  }
 }
 </style>
